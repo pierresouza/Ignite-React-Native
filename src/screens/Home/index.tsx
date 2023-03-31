3d9f6bd7ff856b8d369e40fc45d8f4904d466733
Pierre souza <pas@lionx.com.br>
2023-03-31 17:02:17 -0300

    fix: change setState

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -36,7 +36,7 @@ export function Home() {
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de novembro de 2022</Text>
       <View style={styles.form}>
-        <TextInput style={styles.input} placeholder="Nome do Participante" placeholderTextColor="#6B6B6B" onChangeText={(text) => setParticipantName(text)} value={particpantName} />
+        <TextInput style={styles.input} placeholder="Nome do Participante" placeholderTextColor="#6B6B6B" onChangeText={setParticipantName} value={particpantName} />
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
